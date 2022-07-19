5dffa74b7670d9b896a8c1a12ea426b0eef4fa0d
Guo Gui Wang <guogui.wang@ncsi.com.cn>
2022-07-19 18:21:34 +0800

    20220719

--- py coding.py
+++ py coding.py
@@ -1,19 +1,25 @@
 import pandas as pd
 import numpy as np
 
+# 创建dataframe
 # coords = [("AA", "one"), ("AA", "six"), ("BB", "one"), ("BB", "two"), ("BB", "six")]
 # index = pd.MultiIndex.from_tuples(coords)
 # df = pd.DataFrame([11, 22, 33, 44, 55], index, ["MyData"])
+
 # # 多层index，对level 0进行筛选
 # print(df.xs('AA',level=0,axis=0))
+
 # # 多层index，对level 1进行筛选
 # print(df.xs('six',level=1,axis=0))
+
 # # 多层index，对单行进行筛选
 # print(df.xs(('AA','one')))
 
+# 创建dataframe
 # arrays = [['one','one','one','two','two','two'],[1,2,3,1,2,3]]
 # df = pd.DataFrame(np.random.randn(6,2),index=pd.MultiIndex.from_tuples(list(zip(*arrays))),columns=['A','B'])
 #print(df)
+
 #多层index，对level 0进行筛选
 #print(df.xs('one'))
 
